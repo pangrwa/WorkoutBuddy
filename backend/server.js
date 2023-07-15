0d4db407edcf2d5fcf7f57b345a89f4a5e32805b
Pang Rui Wei <>
2023-07-15 18:13:33 +0800

    add: backend CRUD operations mongoose db

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
+
+// express app
+const app = express();
+
+// middlware 
+app.use(express.json()) // parses incoming requests with JSON payloads 
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to db & listening on port 4000");
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    });
