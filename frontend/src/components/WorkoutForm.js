63f0d45179e474e90816d09dc647a74d5d942075
Pang Rui Wei <>
2023-07-16 20:12:27 +0800

    add: react context for sync

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 
 const WorkoutForm = () => {
+    const { dispatch } = useWorkoutsContext();
     const [title, setTitle] = useState('');
     const [load, setLoad] = useState('');
     const [reps, setReps] = useState('');
@@ -28,6 +30,7 @@ const WorkoutForm = () => {
             setReps('');
             setError(null);
             console.log('new workout added', json);
+            dispatch({type: 'CREATE_WORKOUT', payload: json});
         }
     }
 
